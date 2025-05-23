872968ce1ea5d520240ad06eea2db99d92ffa780
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-05-23 13:44:17 +0530

    Update photoUrl.js

--- src/utils/photoUrl.js
+++ src/utils/photoUrl.js
@@ -1,5 +1,5 @@
 // photoUrl.js
 export const getPhotoUrl = (filename) => {
     if (!filename) return 'https://via.placeholder.com/150?text=No+Image';
-    return `https://gym-server-k6k4.onrender.com/uploads/${filename}`;
+      return `https://sai-fitness-server.onrender.com/uploads/${filename}`;
 };
