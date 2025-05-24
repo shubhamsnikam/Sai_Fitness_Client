12ec6c34f4ea4fa1021186c50e56ec2f32021399
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-05-24 11:31:57 +0530

    Update photoUrl.js

--- src/utils/photoUrl.js
+++ src/utils/photoUrl.js
@@ -1,5 +1,7 @@
-// photoUrl.js
+// Replace this with your real backend URL from Render
+const BASE_URL = 'https://sai-fitness-server.onrender.com';
+
 export const getPhotoUrl = (filename) => {
-    if (!filename) return 'https://via.placeholder.com/150?text=No+Image';
-      return `https://sai-fitness-server.onrender.com/uploads/${filename}`;
+  if (!filename) return 'https://via.placeholder.com/150?text=No+Image';
+  return `${BASE_URL}/uploads/${filename}`;
 };
